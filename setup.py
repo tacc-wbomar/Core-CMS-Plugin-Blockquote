ab64954c3609c9129881cd3a8bb6891be6b28a88
Wesley Bomar <wbomar@tacc.utexas.edu>
2021-10-13 16:18:31 -0500

    Quick: Fix wrong repo URL
    - It was TACC so I would not need to edit this line in the future.
    - But the repo will be renamed also. Must edit later anyway, so fix.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     license='BSD License',
     description='A DjangoCMS plugin (for TACC Core CMS) to render a `<blockquote>` with semantic internal markup.',
     long_description=README,
-    url='https://github.com/TACC/Core-CMS-Plugin-Blockquote/',
+    url='https://github.com/tacc-wbomar/Core-CMS-Plugin-Blockquote/',
     author='Wesley Bomar',
     author_email='wbomar@tacc.utexas.edu',
     # SEE: https://pypi.org/classifiers/
